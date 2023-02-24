86019d880fe7afb6018b08d6e5a1011d76c79d49
Alex Pirko <youzhny_foto@gmail.com>
2023-02-24 10:07:12 +0200

    feat: add Date function

--- Bank-App/script.js
+++ Bank-App/script.js
@@ -7,6 +7,18 @@ const account1 = {
   transactions: [500, 250, -300, 5000, -850, -110, -170, 1100],
   interest: 1.5,
   pin: 1111,
+  transactionsDates: [
+    '2020-10-02T14:43:31.074Z',
+    '2020-10-29T11:24:19.761Z',
+    '2020-11-15T10:45:23.907Z',
+    '2021-01-22T12:17:46.255Z',
+    '2021-02-12T15:14:06.486Z',
+    '2021-03-09T11:42:26.371Z',
+    '2021-05-21T07:43:59.331Z',
+    '2021-06-22T15:21:20.814Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
 const account2 = {
@@ -14,6 +26,18 @@ const account2 = {
   transactions: [2000, 6400, -1350, -70, -210, -2000, 5500, -30],
   interest: 1.3,
   pin: 2222,
+  transactionsDates: [
+    '2020-10-02T14:43:31.074Z',
+    '2020-10-29T11:24:19.761Z',
+    '2020-11-15T10:45:23.907Z',
+    '2021-01-22T12:17:46.255Z',
+    '2021-02-12T15:14:06.486Z',
+    '2021-03-09T11:42:26.371Z',
+    '2021-05-21T07:43:59.331Z',
+    '2021-06-22T15:21:20.814Z',
+  ],
+  currency: 'UAH',
+  locale: 'uk-UA',
 };
 
 const account3 = {
@@ -21,6 +45,18 @@ const account3 = {
   transactions: [900, -200, 280, 300, -200, 150, 1400, -400],
   interest: 0.8,
   pin: 3333,
+  transactionsDates: [
+    '2020-10-02T14:43:31.074Z',
+    '2020-10-29T11:24:19.761Z',
+    '2020-11-15T10:45:23.907Z',
+    '2021-01-22T12:17:46.255Z',
+    '2021-02-12T15:14:06.486Z',
+    '2021-03-09T11:42:26.371Z',
+    '2021-05-21T07:43:59.331Z',
+    '2021-06-22T15:21:20.814Z',
+  ],
+  currency: 'RUB',
+  locale: 'ru-RU',
 };
 
 const account4 = {
@@ -28,6 +64,15 @@ const account4 = {
   transactions: [530, 1300, 500, 40, 190],
   interest: 1,
   pin: 4444,
+  transactionsDates: [
+    '2020-10-02T14:43:31.074Z',
+    '2020-10-29T11:24:19.761Z',
+    '2020-11-15T10:45:23.907Z',
+    '2021-01-22T12:17:46.255Z',
+    '2021-02-12T15:14:06.486Z',
+  ],
+  currency: 'EUR',
+  locale: 'fr-CA',
 };
 
 const account5 = {
@@ -35,6 +80,15 @@ const account5 = {
   transactions: [630, 800, 300, 50, 120],
   interest: 1.1,
   pin: 5555,
+  transactionsDates: [
+    '2020-10-02T14:43:31.074Z',
+    '2020-10-29T11:24:19.761Z',
+    '2020-11-15T10:45:23.907Z',
+    '2021-01-22T12:17:46.255Z',
+    '2021-02-12T15:14:06.486Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
 const accounts = [account1, account2, account3, account4, account5];
@@ -66,10 +120,10 @@ const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
 
-const displayTransactions = function (transactions, sort = false) {
+const displayTransactions = function (account, sort = false) {
   containerTransactions.innerHTML = '';
 
-  const sortedTrans = sort ? transactions.slice().sort((a, b) => a - b) : transactions;
+  const sortedTrans = sort ? account.transactions.slice().sort((a, b) => a - b) : account.transactions;
 
   sortedTrans.forEach((transaction, index) => {
     let transMode;
@@ -79,13 +133,20 @@ const displayTransactions = function (transactions, sort = false) {
       transMode = 'withdrawal'
     }
 
+    const date = new Date(account.transactionsDates[index]);
+    const day = `${date.getDate()}`.padStart(2, '0');
+    const month = `${date.getMonth() + 1}`.padStart(2, '0');
+    const year = date.getFullYear();
+
+    const transDate = `${day}/${month}/${year}`;
+
     const transactionRow = `
       <div class="transactions__row">
         <div class="transactions__type transactions__type--${transMode}">
           ${index + 1} ${transMode}
         </div>
-        <div class="transactions__date">2 дня назад</div>
-        <div class="transactions__value">${transaction}$</div>
+        <div class="transactions__date">${transDate}</div>
+        <div class="transactions__value">${transaction.toFixed(2)}$</div>
       </div>
     `
     containerTransactions.insertAdjacentHTML('afterbegin', transactionRow)
@@ -136,11 +197,14 @@ const displayTotal = function (account) {
 
 const updateUi = function (acc) {
   displayBalance(acc);
-  displayTransactions(acc.transactions);
+  displayTransactions(acc);
   displayTotal(acc);
 }
 
 let currAccount;
+// currAccount = account1;
+// updateUi(currAccount);
+// containerApp.style.opacity = 1;
 
 btnLogin.addEventListener('click', function (e) {
   e.preventDefault();
@@ -150,6 +214,12 @@ btnLogin.addEventListener('click', function (e) {
     containerApp.style.opacity = '1';
 
     labelWelcome.textContent = `Добро пожаловать, ${currAccount.userName.split(' ')[0]}!`;
+
+    const now = new Date();
+    const day = `${now.getDate()}`.padStart(2, '0');
+    const month = `${now.getMonth() + 1}`.padStart(2, '0');
+    const year = now.getFullYear();
+    labelDate.textContent = `${day}/${month}/${year}`;
 
     inputLoginUsername.value = '';
     inputLoginPin.value = '';
@@ -174,6 +244,10 @@ btnTransfer.addEventListener('click', function (e) {
     currAccount.nickname !== userAccount?.nickname) {
     currAccount.transactions.push(-transferAmount);
     userAccount.transactions.push(transferAmount);
+
+    currAccount.transactionsDates.push(new Date().toISOString());
+    userAccount.transactionsDates.push(new Date().toISOString());
+
     updateUi(currAccount);
   }
 })
@@ -186,6 +260,7 @@ btnLoan.addEventListener('click', function (e) {
 
   if (loanAmount > 0 && currAccount.transactions.some(trans => trans > loanAmount * 0.1)) {
     currAccount.transactions.push(loanAmount);
+    currAccount.transactionsDates.push(new Date().toISOString());
     updateUi(currAccount);
   }
   inputLoanAmount.value = '';
@@ -195,7 +270,7 @@ let isSorted = false;
 
 btnSort.addEventListener('click', function (e) {
   e.preventDefault();
-  displayTransactions(currAccount.transactions, !isSorted);
+  displayTransactions(currAccount, !isSorted);
   isSorted = !isSorted;
 })
 
